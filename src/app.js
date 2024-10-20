cc3d6e2d34064669e882719bc95ce96be5575519
Ayush Singh <ayushsingh28082001@gmail.com>
2024-10-20 12:40:48 +0530

    authorized the admin

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
  //creating server 
  const express=require("express")
+ const {adminAuth}=require("./middlewares/auth.js")
  const app=express();
- app.use("/list",(req,res)=>{
-    res.send("JAI SHREE RAM ")
+ app.use("/admin",adminAuth)
+ app.get("/admin/getAllData",(req,res)=>{
+   res.send({firstName:"Ayush",lastName:"Singh"});
  })
  app.listen(3000,()=>{
     console.log("hello server");
