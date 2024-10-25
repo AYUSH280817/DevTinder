a3c1fd985b39478504683d5a6bd0b28f26bbd63e
Ayush Singh <ayushsingh28082001@gmail.com>
2024-10-25 14:19:56 +0530

    dynamic dtat added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 //create server
 const express=require("express");
-const {connectDB}=require("./config/database")
+const connectDB=require("./config/database")
 const app=express();
+const User=require('./models/user')
+
+app.use(express.json());
+
+app.post("/signup",async (req,res)=>{
+  const user=new User(req.body)
+  try{
+    await user.save();
+    res.send("user added succesfully");
+  }catch(err)
+  {
+    res.status(400).send("error saving the data")
+  }
+});
+
+app.get("/user",async(req,res)=>{
+     const toemail=req.body.email;
+     const useremail=await User.find({email:toemail})
+     res.send(useremail)
+})
+
 connectDB()
 .then(()=>{
 console.log("Database connection established..");
@@ -11,6 +32,8 @@ app.listen(3000,()=>{
 }).catch((err)=>{
 console.log("Database cannot be connected");
 })
+
+
 
 
 
